dafe0eb01f852b784f67523c6fc17ee538e37b3b
d-Aniello <dani.swiatowski@aol.com>
2022-03-06 08:41:47 +0100

    Field model developed

--- Vivello/VivelloApp/models.py
+++ Vivello/VivelloApp/models.py
@@ -12,6 +12,8 @@ class Field(models.Model):
     name = models.CharField(max_length=50)
     area = models.IntegerField()
     location = models.CharField(max_length=100)
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
+    crop = models.ManyToManyField('Crop', blank=True)
 
 
 class VehicleType(models.Model):
